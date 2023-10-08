5bb93ab0fe4a1bbb8876018eb85fc80810fb8caf
George-Gyang <georgegyang3@gmail.com>
2023-10-09 00:06:04 +0100

    modify UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useRef } from "react"
 import { Button, Form } from "react-bootstrap"
+import imgs from "./assets/images-.jpg"
 
 
 function App() {
 
-const inputValue = useRef(null)
+  const inputValue = useRef(null)
 
-const handleSubmit = (e)=>{
-  e.preventDefault()
-  console.log(inputValue.current.value)
-}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputValue.current.value)
+  }
 
-const handleClick = (selectChoice)=>{
-  inputValue.current.value = selectChoice
-}
+  const handleClick = (selectChoice) => {
+    inputValue.current.value = selectChoice
+  }
+
+  const Api_Url = "https://api.unsplash.com/search/photos"
+
+  const IMAGE_ON_PAGE = 20
 
   return (
     <div>
@@ -25,7 +30,7 @@ const handleClick = (selectChoice)=>{
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Control type="text" ref={inputValue} placeholder="Search" />
                 <Form.Text className="text-muted">
-                 Search your favorite category
+                  Search your favorite category
                 </Form.Text>
               </Form.Group>
             </Form>
@@ -33,11 +38,55 @@ const handleClick = (selectChoice)=>{
         </div>
         <div className="d-flex justify-content-center">
           <div className="col-md-8 d-flex  justify-content-between">
-            <Button onClick={( )=> handleClick("nature")} >Nature</Button>
-            <Button onClick={( )=> handleClick("Scieence")} >Science</Button>
-            <Button onClick={( )=> handleClick("Animation")} >Animation</Button>
-            <Button onClick={( )=> handleClick("Vacaktion")} >Vacaktion</Button>
-            <Button onClick={( )=> handleClick("Industries")} >Industries</Button>
+            <Button onClick={() => handleClick("nature")} >Nature</Button>
+            <Button onClick={() => handleClick("Scieence")} >Science</Button>
+            <Button onClick={() => handleClick("Animation")} >Animation</Button>
+            <Button onClick={() => handleClick("Vacaktion")} >Vacaktion</Button>
+            <Button onClick={() => handleClick("Industries")} >Industries</Button>
+          </div>
+        </div>
+        <div className="row my-5">
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
+          </div>
+          <div className="col-lg-3 mb-4 col-md-4 col-6">
+            <img src={imgs} alt="" className="img-fluid ww-100" />
           </div>
         </div>
       </div>
