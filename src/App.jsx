32460e3667c019e6d901d4800401d10f1093ceda
George-Gyang <georgegyang3@gmail.com>
2023-10-09 13:58:51 +0100

    implement api

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { Button, Form } from "react-bootstrap"
 import imgs from "./assets/images-.jpg"
+import axios from "axios"
 
 
 function App() {
 
   const inputValue = useRef(null)
+  const [images, setImages] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(inputValue.current.value)
+    getImage();
   }
 
   const handleClick = (selectChoice) => {
     inputValue.current.value = selectChoice
+    getImage();
   }
 
-  const Api_Url = "https://api.unsplash.com/search/photos"
+  const ApiUrl = "https://api.unsplash.com/search/photos";
 
-  const IMAGE_ON_PAGE = 20
+  const ImagesPerPage = 20;
+
+  const getImage = async () => {
+    try {
+      const response = await axios.get(`${ApiUrl}?query=${inputValue.current.value}&page=1&per_page=${ImagesPerPage}&client_id=${import.meta.env.VITE_API_KEY}`);
+      setImages(response.data.results)
+      setTotalPages(response.data.total_pages)
+    } catch (e) {
+      console.log(e)
+    }
+  };
 
   return (
     <div>
@@ -39,55 +54,23 @@ function App() {
         <div className="d-flex justify-content-center">
           <div className="col-md-8 d-flex  justify-content-between">
             <Button onClick={() => handleClick("nature")} >Nature</Button>
-            <Button onClick={() => handleClick("Scieence")} >Science</Button>
+            <Button onClick={() => handleClick("Science")} >Science</Button>
             <Button onClick={() => handleClick("Animation")} >Animation</Button>
-            <Button onClick={() => handleClick("Vacaktion")} >Vacaktion</Button>
+            <Button onClick={() => handleClick("Vacation")} >Vacation</Button>
             <Button onClick={() => handleClick("Industries")} >Industries</Button>
           </div>
         </div>
         <div className="row my-5">
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
-          <div className="col-lg-3 mb-4 col-md-4 col-6">
-            <img src={imgs} alt="" className="img-fluid ww-100" />
-          </div>
+          {
+            images.map((image) => (
+              <div key={image.id} className="col-lg-3 mb-4 col-md-4 col-6">
+                <img
+                  src={image.urls.small}
+                  alt={image.alt_description} className="img-fluid ww-100" />
+              </div>
+            )
+            )
+          }
         </div>
       </div>
     </div>
